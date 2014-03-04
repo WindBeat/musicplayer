1fd85e5abcf67df14e5691d28e0e9e39a8e8eb8a
WindBeat <jordanwalker465@gmail.com>
2014-03-04 11:56:55 -0800

    testing json

--- soundcloud.js
+++ soundcloud.js
@@ -9,7 +9,7 @@ function playSomeMusic(myGenre) {
       var random = Math.floor(Math.random() * 49);
       var track_url = tracks[random].permalink_url;
       //Displays the sound player
-      SC.oEmbed(track_url, {auto_play: true, color: "ff0066", maxheight: 130, maxwidth: 500, show_comments:false },
+      SC.oEmbed(track_url, {auto_play: true, color: "ff0066", maxheight: 130, maxwidth: 500, show_comments:false, format=json },
         document.getElementById("player"));
       }
 );
