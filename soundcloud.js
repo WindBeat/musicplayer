dfac45e8b6b84678c38f7b9c70dddec274c05082
WindBeat <jordanwalker465@gmail.com>
2014-03-06 13:09:16 -0800

    testing json

--- soundcloud.js
+++ soundcloud.js
@@ -1,21 +1,17 @@
 
-
 function playSomeMusic(myGenre) {  
 	SC.initialize({      
   client_id: '02f6c269172ac9312f9de8ac41324001'
 });
-
-	SC.get('/tracks', { genres: myGenre, bpm: { from: 120 } },
+  var url = 'https://api.soundcloud.com/tracks.json?client_id=02f6c269172ac9312f9de8ac41324001';
+	$.getJSON(url, { genres: myGenre, bpm: { from: 120 } },
   function(tracks) {
-    //FIRST soundcloud sends back an array of 50 objects (0~49)
-    //so let's randomize which one we play:
-      //this logic returns a number between 0 and 50
+    //Pulls 50 songs and then randomly chooses one
       var random = Math.floor(Math.random() * 49);
       var track_url = tracks[random].permalink_url;
-      //LAST let's use the oEmbed method to display the soundplayer
-      //in the element #target with a options such as auto play
-      SC.oEmbed(track_url, {auto_play: true, color: "ff0066"},
-        document.getElementById("target"));
+      //Displays the sound player
+      SC.oEmbed(track_url, {auto_play: true, color: "ff0066", maxheight: 130, maxwidth: 500, show_comments:false,},
+        document.getElementById("player"));
       }
 );
 };
